392321fc7cd899bb0e4ccbe50da837497475bd22
piiidro <pidroaourra@gmail.coom>
2023-09-11 11:43:08 -0700

    first commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,29 +1,33 @@
-#include <unistd.h>
+#include <stdio.h>
 
-void print_combinations() {
-    int digit1, digit2, digit3;
+int main() {
+    for (int i = 0; i < 100; i++) {
+        for (int j = i; j < 100; j++) {
+            // Print the first two-digit number
+            putchar((i / 10) + '0');
+            putchar((i % 10) + '0');
 
-    for (digit1 = 0; digit1 <= 7; digit1++) {
-        for (digit2 = digit1 + 1; digit2 <= 8; digit2++) {
-            for (digit3 = digit2 + 1; digit3 <= 9; digit3++) {
-                // Print the digits as characters
-                putchar(digit1 + '0');
-                putchar(digit2 + '0');
-                putchar(digit3 + '0');
+            // Print a space
+            putchar(' ');
 
-                // Check if we need to print a comma and space
-                if (digit1 != 7 || digit2 != 8 || digit3 != 9) {
-                    putchar(',');
-                    putchar(' ');
-                }
+            // Print the second two-digit number
+            putchar((j / 10) + '0');
+            putchar((j % 10) + '0');
+
+            // Print a comma and a space if not the last combination
+            if (i != 99 || j != 99) {
+                putchar(',');
+                putchar(' ');
             }
         }
     }
-    putchar('\n');
-}
 
-int main() {
-    print_combinations();
     return 0;
 }
+This code uses nested loops to iterate through all possible combinations of two two-digit numbers, ensuring they are printed with the specified formatting. It also takes care of not printing the comma and space after the last combination.
+
+
+
+
+
 
