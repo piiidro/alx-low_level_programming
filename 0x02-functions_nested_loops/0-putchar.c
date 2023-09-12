57c2863c36e1b77a888cff4a126aabb043a15ded
piiidro <pidroaourra@gmail.coom>
2023-09-12 10:08:39 -0700

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,17 @@
-#include <stdio.h>
-int main() {
-	printf("_putchar\n");
-	return 0;
-           }
+#include "main.h"
+/**
+ * main - entry point
+ * description: prints _putchar word
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	for (int i = 0 ; i <= 8 ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
